a0e99ac3da1fe1b78b00e32aced05597d996508d
mario-L-code <mario.lipan@gmail.com>
2025-05-14 06:52:14 +0000

    Fixed dockerfile. Used microdnf, not dnf

--- fetch_title.sh
+++ fetch_title.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Curl the REST API 
 RESPONSE=$(curl -s https://httpbin.org/json)
@@ -6,4 +6,5 @@ RESPONSE=$(curl -s https://httpbin.org/json)
 # Extract the title
 TITLE=$(echo "$RESPONSE" | jq -r '.slideshow.title')
 
+# Echo the title to console
 echo "Slideshow title: $TITLE"
